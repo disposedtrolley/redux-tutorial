7d876649f756a056fd063cfcb022cba0aae2eba7
jamesliu <jamesliu@au1.ibm.com>
2018-02-16 21:52:52 +1100

    add error handling

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,12 +1,22 @@
 import $ from 'jquery'
 
 export const UPDATE_USER = 'users:updateUser'
+export const SHOW_ERROR = 'users:showError'
 
 export function updateUser(newUser) {
 	return {
 		type: UPDATE_USER,
 		payload: {
 			user: newUser
+		}
+	}
+}
+
+export function showError() {
+	return {
+		type: SHOW_ERROR,
+		payload: {
+			user: 'ERROR!'
 		}
 	}
 }
@@ -20,6 +30,8 @@ export function apiRequest() {
 			},
 			error() {
 				console.log('ERROR')
+
+				dispatch(showError())
 			}
 		})
 	}
--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,9 +1,11 @@
-import { UPDATE_USER } from '../actions/user-actions'
+import { UPDATE_USER, SHOW_ERROR } from '../actions/user-actions'
 
 // { type, payload } is ES6 destructuring for the action parameter
 export default function userReducer(state = '', { type, payload }) {
 	switch(type) {
 		case UPDATE_USER:
+			return payload.user
+		case SHOW_ERROR:
 			return payload.user
 		default:
 			return state
