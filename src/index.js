1d0d375b5abd4d89ba8028400f4cee97efaf4fe0
jamesliu <jamesliu@au1.ibm.com>
2018-02-16 19:35:47 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { combinedReducers, createStore } from 'redux'
+
+function reducer(state, action) {
+	if (action.type === 'changeState') {
+		return action.payload.newState
+	}
+	
+	return 'State'
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+
+const action = {
+	type: 'changeState',
+	payload: {
+		newState: 'New state'
+	}
+}
+
+store.dispatch(action)
+
+console.log(store.getState())
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
